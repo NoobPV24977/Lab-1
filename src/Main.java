5ce54902d7a4044e71e3d07162d6a750b8688e4b
NoobPV24977 <noob24977@gmail.com>
2025-04-06 20:39:57 +0300

    Lab 1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * This is the HelloWorldApp class.
+ */
+public class Main {
+    /**
+     * This is the main method that prints "Hello, World!" to the console.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
